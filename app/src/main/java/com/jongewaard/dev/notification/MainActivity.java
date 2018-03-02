9be807aefd11d37d6aa57834102344e813e3d000
german <germanjongewaard@gmail.com>
2018-03-02 12:18:29 +0100

    Commit

--- app/src/main/java/com/jongewaard/dev/notification/MainActivity.java
+++ app/src/main/java/com/jongewaard/dev/notification/MainActivity.java
@@ -1,13 +1,16 @@
 package com.jongewaard.dev.notification;
 
+import android.app.Notification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
@@ -25,37 +28,39 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.buttonSend)
     Button buttondSend;
 
-    private boolean idHighImportance = false;
+    @BindString(R.string.switch_notifications_on) String switchTextOn;
+    @BindString(R.string.switch_notifications_off) String switchTextOff;
 
+    private boolean isHighImportance = false;
+    private NotificationHandler notificationHandler;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this); //Right after setContentView
-
+        ButterKnife.bind(this); // Right after setContentView
+        notificationHandler = new NotificationHandler(this);
     }
 
-    @OnClick(R.id.buttonSend)
-    public void click(){
 
-        sendNotification();
-
-    }
 
     @OnCheckedChanged(R.id.switchImportance)
     public void change(CompoundButton buttonView, boolean isChecked){
 
-        idHighImportance = isChecked;
-
-        Toast.makeText(this, "Working " + idHighImportance, Toast.LENGTH_SHORT).show();
-
+        isHighImportance = isChecked;
+        switchImportance.setText((isChecked) ? switchTextOn : switchTextOff);
     }
 
 
     private void sendNotification(){
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
 
-        Toast.makeText(this, "Working", Toast.LENGTH_SHORT).show();
+        if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(message)) {
+            Notification.Builder nb = notificationHandler.createNotification(title, message, isHighImportance);
+           /* notificationHandler.getManager().notify(++counter, nb.build());
+            notificationHandler.publishNotificationSummaryGroup(isHighImportance);*/
+        }
     }
 }
