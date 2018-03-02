ca2fa6a7186d789ac3dc3de928b31c897725d396
german <germanjongewaard@gmail.com>
2018-03-02 10:43:46 +0100

    Commit

--- app/src/main/java/com/jongewaard/dev/notification/MainActivity.java
+++ app/src/main/java/com/jongewaard/dev/notification/MainActivity.java
@@ -2,13 +2,53 @@ package com.jongewaard.dev.notification;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 
 //WIN => CTRL + SHIFT + N -- Find file!
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTextTitle)
+    EditText editTextTitle;
+    @BindView(R.id.editTextMessage)
+    EditText editTextMessage;
+    @BindView(R.id.switchImportance)
+    Switch switchImportance;
+    @BindView(R.id.buttonSend)
+    Button buttondSend;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this); //Right after setContentView
+
+    }
+
+    @OnClick(R.id.buttonSend)
+    public void click(){
+
+        sendNotification();
+
+    }
+
+    @OnCheckedChanged(R.id.switchImportance)
+    public void change(CompoundButton buttonView, boolean isChecked){
+
+
+    }
+
+
+    private void sendNotification(){
+
     }
 }
